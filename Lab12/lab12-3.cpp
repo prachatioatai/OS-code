67c382bf9e7352779d4b3d51ab4da1aad8db9258
prachatioatai <prachatipatai2006@gmail.com>
2026-03-08 22:56:37 +0700

    Create lab12-3.cpp

---
+++ Lab12/lab12-3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ 
+int main() {
+    // Sequential Write
+    ofstream out("records.bin", ios::binary);
+    int values[] = {100, 200, 300, 400, 500};
+    for (int i = 0; i < 5; i++) {
+        out.write(reinterpret_cast<char*>(&values[i]), sizeof(int));
+    }
+    out.close();
+ 
+    // Sequential Read
+    ifstream in("records.bin", ios::binary);
+    int val;
+    int pos = 0;
+    cout << "Sequential Read:" << endl;
+    while (in.read(reinterpret_cast<char*>(&val), sizeof(int))) {
+        cout << "Record[" << pos++ << "] = " << val << endl;
+    }
+    in.close();
+    return 0;
+}
